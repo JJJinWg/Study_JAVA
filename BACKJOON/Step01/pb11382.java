c62898120eccb530a498879d4886d2aed1251682
jin <jin14410@naver.com>
2025-11-27 16:24:26 +0900

    11/27

---
+++ BACKJOON/Step01/pb11382.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class pb11382 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        long A = in.nextLong();
+        long B = in.nextLong();
+        long C = in.nextLong();
+        in.close();
+        System.out.println(A + B + C);
+    }
+}
